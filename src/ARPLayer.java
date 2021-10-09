76e9d7cc2936b49f7dac737aea81754831d1e1e7
Dudu-CNU <k0s0a7@gmail.com>
2021-10-09 18:20:51 +0900

    ARP Layer 수정

--- src/ARPLayer.java
+++ src/ARPLayer.java
@@ -1,93 +1,120 @@
-public class ARPLayer implements BaseLayer{
-        private class _ARP_HEADER {
-            byte[] macType;								// Hardware Type
-            byte[] ipType;								// Protocol Type
-            byte macAddrLen;							// Length of hardware Address
-            byte ipAddrLen;								// Length of protocol Address
-            byte[] opcode;								// Opcode (ARP Request)
-            _ETHERNET_ADDR srcMac;						// Sender's hardware Address
-            _IP_ADDR srcIp;								// Sender's protocol Address
-            _ETHERNET_ADDR dstMac;						// Target's hardware Address
-            _IP_ADDR dstIp;								// Target's protocol Address
-            
-            public _ARP_HEADER() {						// 28 Bytes
-                this.macType = new byte[2];				// 2 Bytes / 0 ~ 1
-                this.ipType = new byte[2];				// 2 Bytes / 2 ~ 3
-                this.macAddrLen = (byte) 0x00;			// 1 Byte  / 4
-                this.ipAddrLen = (byte) 0x00;			// 1 Byte  / 5
-                this.opcode = new byte[2];				// 2 Bytes / 6 ~ 7 
-                this.srcMac = new ETHERNET_ADDR();		// 6 Bytes / 8 ~ 13 
-                this.srcIp = new IP_ADDR();			// 4 Bytes / 14 ~ 17
-                this.dstMac = new ETHERNET_ADDR();		// 6 Bytes / 18 ~ 23
-                this.dstIp = new IP_ADDR();			// 4 Bytes / 24 ~ 27
-            }
-        }
+import java.util.ArrayList;
 
-        public static class _ARP_Cache_Entry{    
-            byte[] addr;
-            String status;
-            String arp_interface;
-            //ARP Cache Entry
-            public _ARPCache_Entry(byte[] addr, String status, String arp_interface){
-                this.addr = addr;
-                this.status = status;
-                this.arp_interface = arp_interface;
-            }
-        }
-        //Proxy ARP Entry
-        public static class _Proxy_Entry{
-            String hostName;
-            byte[] addr;
+public class ARPLayer implements BaseLayer {
 
-            public _Proxy_Entry(byte[] addr, String hostName){
-                this.hostName = hostName;
-                this.addr = addr;
-            }
-        }
+	public int nUpperLayerCount = 0;
+	public String pLayerName = null;
+	public BaseLayer p_UnderLayer = null;
+	public ArrayList<BaseLayer> p_aUpperLayer = new ArrayList<BaseLayer>();
+	_ARP_Frame ARPframe;
+	
+	public ARPLayer(String pName) {
+		// TODO Auto-generated constructor stub
+		pLayerName = pName;
+	}
+	
+	public void ResetFrame() {
+		ARPframe = new _ARP_Frame();
+	}
+	
+	private class _ETHERNET_ADDR {
+        private byte[] addr = new byte[6];
 
-        private void ResetHeader(){
-                m_sHeader = new ARP_HEADER();
-        }
+        public _ETHERNET_ADDR() {
+            this.addr[0] = (byte) 0x00;
+            this.addr[1] = (byte) 0x00;
+            this.addr[2] = (byte) 0x00;
+            this.addr[3] = (byte) 0x00;
+            this.addr[4] = (byte) 0x00;
+            this.addr[5] = (byte) 0x00;
 
-        public ARPLayer(String Name){
-            pLayerName = pName;
-            ResetHeader();    
-        }
-        
-        // 각 Port의 mac Address와 Ip Address 저장하는 함수
-        public void initAddress() {
-            String port0_mac = NILayer.getMacAddress(0);
-            String port1_mac = NILayer.getMacAddress(1);
-            myMacAddress[0] = Translator.macToByte(port0_mac);
-            myMacAddress[1] = Translator.macToByte(port1_mac);
-            
-            String port0_ip = NILayer.getIpAddress(0);
-            String port1_ip = NILayer.getIpAddress(1);
-            myIpAddress[0] = Translator.ipToByte(port0_ip);
-            myIpAddress[1] = Translator.ipToByte(port1_ip);
-        }
-        
-        private class _IP_ADDR {
-            private byte[] addr = new byte[4];
-            
-            public _IP_ADDR() {
-                this.addr[0] = (byte) 0x00;
-                this.addr[1] = (byte) 0x00;
-                this.addr[2] = (byte) 0x00;
-                this.addr[3] = (byte) 0x00;
-            }
-        }
-        
-        private class _ETHERNET_ADDR {
-            private byte[] addr = new byte[6];
-            
-            public _ETHERNET_ADDR() {
-                this.addr[0] = (byte) 0x00;
-                this.addr[1] = (byte) 0x00;
-                this.addr[2] = (byte) 0x00;
-                this.addr[3] = (byte) 0x00;
-                this.addr[4] = (byte) 0x00;
-                this.addr[5] = (byte) 0x00;
-            }
         }
     }
+	
+	private class _IP_ADDR {
+		private byte[] addr = new byte[4];
+		
+		public _IP_ADDR() {
+			this.addr[0] = (byte) 0x00;
+            this.addr[1] = (byte) 0x00;
+            this.addr[2] = (byte) 0x00;
+            this.addr[3] = (byte) 0x00;
+		}
+	}
+	
+	private class _ARP_Frame {
+		byte[] hard_type;
+		byte[] prot_type;
+		byte hard_size;
+		byte prot_size;
+		byte[] op;
+		_ETHERNET_ADDR src_enet_addr;
+		_IP_ADDR src_ip_addr;
+		_ETHERNET_ADDR dst_enet_addr;
+		_IP_ADDR dst_ip_addr;
+		
+		public _ARP_Frame() {
+			this.hard_type = new byte[2];
+			this.prot_type = new byte[2];
+			this.hard_size = 0x00;
+			this.prot_size = 0x00;
+			this.op = new byte[2];
+			this.src_enet_addr = new _ETHERNET_ADDR();
+			this.src_ip_addr = new _IP_ADDR();
+			this.dst_enet_addr = new _ETHERNET_ADDR();
+			this.dst_ip_addr = new _IP_ADDR();
+		}
+	}
+	
+	@Override
+	public String GetLayerName() {
+		return pLayerName;
+	}
+
+	@Override
+	public BaseLayer GetUnderLayer() {
+		if (p_UnderLayer == null)
+			return null;
+		return p_UnderLayer;
+	}
+
+	@Override
+	public BaseLayer GetUpperLayer(int nindex) {
+		if (nindex < 0 || nindex > nUpperLayerCount || nUpperLayerCount < 0)
+			return null;
+		return p_aUpperLayer.get(nindex);
+	}
+
+	@Override
+	public void SetUnderLayer(BaseLayer pUnderLayer) {
+		if (pUnderLayer == null)
+			return;
+		this.p_UnderLayer = pUnderLayer;
+	}
+
+	@Override
+	public void SetUpperLayer(BaseLayer pUpperLayer) {
+		if (pUpperLayer == null)
+			return;
+		this.p_aUpperLayer.add(nUpperLayerCount++, pUpperLayer);
+	}
+
+	@Override
+	public void SetUpperUnderLayer(BaseLayer pUULayer) {
+		this.SetUpperLayer(pUULayer);
+		pUULayer.SetUnderLayer(this);
+	}
+	
+	public boolean Send() {
+		
+		
+		return true;
+	}
+	
+	public boolean Recieve() {
+		
+		
+		return true;
+	}
+
+}
